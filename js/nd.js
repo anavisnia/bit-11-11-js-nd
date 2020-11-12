d96fe7a464ecbded9421050068ab9fff9b3a2f1b
anavisnia <73888794+anavisnia@users.noreply.github.com>
2020-11-12 19:17:03 +0200

    Kintamuju palyginimas done

--- js/nd.js
+++ js/nd.js
@@ -51,11 +51,122 @@ if (num1 <= num2) {
     console.log(`${num1} <= ${num2}`);
     console.log('False');
 };
-
+console.log('------------------------------------');
 // Išvesti teksto tipo kintamųjų ilgius
 const myText = 'You will do it!';
 const myText2 = 'Do not overthink, you are doing well';
 console.log(myText.length);
 console.log(myText2.length);
-
+console.log('------------------------------------');
 // Tarpusavyje palyginti teksto tipo kintamųjų ilgius:
+const textLeng1 = myText.length;
+console.log(textLeng1);
+const textLeng2 = myText2.length;
+console.log(textLeng2);
+
+if (textLeng1 > textLeng2) {
+    console.log(`${textLeng1} > ${textLeng2}`);
+    console.log('True');
+} else {
+    console.log(`${textLeng1} > ${textLeng2}`);
+    console.log('False');
+};
+
+if (textLeng1 < textLeng2) {
+    console.log(`${textLeng1} < ${textLeng2}`);
+    console.log('True');
+} else {
+    console.log(`${textLeng1} < ${textLeng2}`);
+    console.log('False');
+};
+
+if (textLeng1 === textLeng2) {
+    console.log(`${textLeng1} === ${textLeng2}`);
+    console.log('True');
+} else {
+    console.log(`${textLeng1} === ${textLeng2}`);
+    console.log('False');
+};
+
+if (textLeng1 !== textLeng2) {
+    console.log(`${textLeng1} !== ${textLeng2}`);
+    console.log('True');
+} else {
+    console.log(`${textLeng1} !== ${textLeng2}`);
+    console.log('False');
+};
+
+if (textLeng1 >= textLeng2) {
+    console.log(`${textLeng1} >= ${textLeng2}`);
+    console.log('True');
+} else {
+    console.log(`${textLeng1} >= ${textLeng2}`);
+    console.log('False');
+};
+
+if (textLeng1 <= textLeng2) {
+    console.log(`${textLeng1} <= ${textLeng2}`);
+    console.log('True');
+} else {
+    console.log(`${textLeng1} <= ${textLeng2}`);
+    console.log('False');
+};
+console.log('------------------------------------');
+// Išvesti sąrašo tipo kintamųjų ilgius
+const favSongs = ['Deja vu', 'Jazz Bar', 'Colours', 'Truth', 'Graveyard', 'Criminal'];
+const favSingers = ['DreamCatcher', 'JB', 'TS', 'AL', 'Nightly'];
+
+const favSongsLeng = favSongs.length;
+console.log(favSongsLeng);
+const favSingersLeng = favSingers.length;
+console.log(favSingersLeng);
+console.log('------------------------------------');
+// Tarpusavyje palyginti sąrašo tipo kintamųjų ilgius:
+if (favSongsLeng > favSingersLeng) {
+    console.log(`${favSongsLeng} > ${favSingersLeng}`);
+    console.log('True');
+} else {
+    console.log(`${favSongsLeng} > ${favSingersLeng}`);
+    console.log('False');
+};
+
+if (favSongsLeng < favSingersLeng) {
+    console.log(`${favSongsLeng} < ${favSingersLeng}`);
+    console.log('True');
+} else {
+    console.log(`${favSongsLeng} < ${favSingersLeng}`);
+    console.log('False');
+};
+
+if (favSongsLeng === favSingersLeng) {
+    console.log(`${favSongsLeng} === ${favSingersLeng}`);
+    console.log('True');
+} else {
+    console.log(`${favSongsLeng} === ${favSingersLeng}`);
+    console.log('False');
+};
+
+if (favSongsLeng !== favSingersLeng) {
+    console.log(`${favSongsLeng} !== ${favSingersLeng}`);
+    console.log('True');
+} else {
+    console.log(`${favSongsLeng} !== ${favSingersLeng}`);
+    console.log('False');
+};
+
+if (favSongsLeng >= favSingersLeng) {
+    console.log(`${favSongsLeng} >= ${favSingersLeng}`);
+    console.log('True');
+} else {
+    console.log(`${favSongsLeng} >= ${favSingersLeng}`);
+    console.log('False');
+};
+
+if (favSongsLeng <= favSingersLeng) {
+    console.log(`${favSongsLeng} <= ${favSingersLeng}`);
+    console.log('True');
+} else {
+    console.log(`${favSongsLeng} <= ${favSingersLeng}`);
+    console.log('False');
+};
+
