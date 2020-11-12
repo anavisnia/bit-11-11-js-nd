ba8cf06f5b0ed048d928dcf38b55ac037d2a0d74
anavisnia <73888794+anavisnia@users.noreply.github.com>
2020-11-12 18:59:13 +0200

    Initial commit of nd.js

---
+++ js/nd.js
@@ -0,0 +1,61 @@
+/* *******************
+*Kintamųjų palyginimas
+******************* */
+
+// Tarpusavyje palyginti skaičiaus tipo kintamuosius:
+const num1 = 5;
+const num2 = 12;
+
+if (num1 > num2) {
+    console.log('True');
+} else {
+    console.log(`${num1} > ${num2}`);
+    console.log('False');
+};
+
+if (num1 < num2) {
+    console.log(`${num1} < ${num2}`);
+    console.log('True');
+} else {
+    console.log('False');
+};
+
+if (num1 === num2) {
+    console.log(`${num1} === ${num2}`);
+    console.log('True');
+} else {
+    console.log(`${num1} === ${num2}`);
+    console.log('False');
+};
+
+if (num1 !== num2) {
+    console.log(`${num1} !== ${num2}`);
+    console.log('True');
+} else {
+    console.log(`${num1} !== ${num2}`);
+    console.log('False');
+};
+
+if (num1 >= num2) {
+    console.log(`${num1} >= ${num2}`);
+    console.log('True');
+} else {
+    console.log(`${num1} >= ${num2}`);
+    console.log('False');
+};
+
+if (num1 <= num2) {
+    console.log(`${num1} <= ${num2}`);
+    console.log('True');
+} else {
+    console.log(`${num1} <= ${num2}`);
+    console.log('False');
+};
+
+// Išvesti teksto tipo kintamųjų ilgius
+const myText = 'You will do it!';
+const myText2 = 'Do not overthink, you are doing well';
+console.log(myText.length);
+console.log(myText2.length);
+
+// Tarpusavyje palyginti teksto tipo kintamųjų ilgius:
