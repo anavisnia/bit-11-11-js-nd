962f314c7a466d0be684c1bb01cd1c6f9906297b
anavisnia <73888794+anavisnia@users.noreply.github.com>
2020-11-16 14:58:38 +0200

    backward

--- js/ndClass.js
+++ js/ndClass.js
@@ -25,3 +25,31 @@ if (nuo < iki) {
 
 
 console.log(`Skaičių intervale tarp ${nuo} ir ${iki} yra ${sum}`);
+
+// backward
+
+const text = 'abcdef';
+let back = '';
+
+// for (let i = text.length - 1; i >= 0; i--) {
+//     const letter = text[i];
+//     console.log(i, letter);
+//     back = back + letter;
+// };
+
+// for (let i = 1; i <= text.length; i++) {
+//     const letter = text[text.length - i];
+//     back = back + letter;
+// };
+
+// for (let i = 0; i < text.length; i++) {
+//     const letter = text[text.length - i - 1];
+//     back = back + letter;
+// };
+
+for (let i = 0; i < text.length; i++) {
+    const letter = text[i];
+    back = letter + back;
+};
+
+console.log(text, '->', back);
